13cd123d117d2be13ca7bc327f12bc5eb7f52478
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-10 20:04:45 -0300

    feat: create new funcition for Todo form an logic of the submit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="todo-main">
-   <h1>Todo App</h1>
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,15 +1,29 @@
 import React, {useState} from 'react'
 
-function TodoForm () {
-  const [input, setInpunt] = useState('')
+function TodoForm (props) {
+  const [input, setInput] = useState('')
+
+  const handleChange = e => {
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    })
+    setInput('')
+  }
   return (
-   <form className="todo-form">
+   <form className="todo-form" onSubmit={handleSubmit}>
      <input 
       type="text" 
-      palceholder="Adicione uma Tarefa" 
+      placeholder="Adicione uma Tarefa" 
       value={input} 
       name="text" 
       className="todo-input"
+      onChange={handleChange}
       />
       <button className="todo-button">Adicionar</button>
    </form>
