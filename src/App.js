35f0c267d462a3ba85b6d6dca2f9b07b4daeb2bf
lfeliperibeiro <piporibeiro@gmail.com>
2020-10-10 21:26:13 -0300

    feat: create footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
+    <>
     <div className="todo-main">
       <TodoList />
     </div>
+      <div className="footer">Feito com por 💙  Felipe Ribeiro</div>
+      </>
   );
 }
 
